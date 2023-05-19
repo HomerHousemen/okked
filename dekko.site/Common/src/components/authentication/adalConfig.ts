bb13c7ca075d2884937a6b64f1533788169b8f94
Homer Housemen <homer.housemen@protonmail.com>
2023-05-19 11:58:24 -0400

    Anonymize Data

--- dekko.site/Common/src/components/authentication/adalConfig.ts
+++ dekko.site/Common/src/components/authentication/adalConfig.ts
@@ -2,24 +2,24 @@ import { AuthenticationContext, AdalConfig } from "react-adal";
 
 const hostname = window && window.location && window.location.hostname;
 const isLocal = hostname === "localhost";
-const isDev = hostname.includes("caanderson");
+const isDev = hostname.includes("name");
 const tenant =
   isLocal || isDev
-    ? "chriscaanderson.onmicrosoft.com"
-    : "naproperties.onmicrosoft.com";
+    ? "username.onmicrosoft.com"
+    : "company.onmicrosoft.com";
 const clientId =
   isLocal || isDev
     ? "628d442b-7422-4575-b2fb-a512dfe107c6"
     : "27acdee5-e7e5-4ff7-9cca-87c9fcc2c9d9";
 const api =
   isLocal || isDev
-    ? "https://chriscaanderson.onmicrosoft.com/930eed0d-3045-4635-a47b-843de1324452"
-    : "https://naproperties.onmicrosoft.com/27acdee5-e7e5-4ff7-9cca-87c9fcc2c9d9";
+    ? "https://username.onmicrosoft.com/930eed0d-3045-4635-a47b-843de1324452"
+    : "https://company.onmicrosoft.com/27acdee5-e7e5-4ff7-9cca-87c9fcc2c9d9";
 const redirectUri = isLocal
   ? process.env.LOCALHOST_URL
   : isDev
-  ? "https://www.caanderson.llc:5001"
-  : "https://dekko.naproperties.com";
+  ? "https://www.name.llc:5001"
+  : "https://dekko.company.com";
 
 export const adalConfig: AdalConfig = {
   tenant: tenant,
